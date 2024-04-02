a033cf307eabca02c2dd0516e985e7053a3900ad
wanjala stephen <stephenwanjala145@gmail.com>
2024-04-02 12:36:15 +0300

    waste summary

--- wasteman/views.py
+++ wasteman/views.py
@@ -1,9 +1,20 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from wasteman.models import Waste, Schedule, IssueReport
+
 
 # Create your views here.
 
 @login_required(login_url='login')
 def home(request):
-    return render(request=request, template_name='wasteman/home.html')
+    waste_summary = Waste.objects.all()[:5]  # Get the latest 5 waste entries
+    recent_issues = IssueReport.objects.all()[:5]  # Get the latest 5 issue reports
+    upcoming_schedules = Schedule.objects.all()[:5]  # Get the first 5 upcoming pickup schedules
+
+    context = {
+        'waste_summary': waste_summary,
+        'recent_issues': recent_issues,
+        'upcoming_schedules': upcoming_schedules,
+    }
+    return render(request=request, template_name='wasteman/home.html',context=context)
